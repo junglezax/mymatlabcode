d8e1dc983cf639eca8a6519e2a9be089bb08b1b6
nullspace <jxhchina@gmail.com>
2014-11-03 12:46:38 +0800

    load bad

--- DL_chair/load_it.m
+++ DL_chair/load_it.m
@@ -6,10 +6,11 @@ img_resized = {};
 x = [];
 labels = [];
 fns = {};
+bad = {};
 
 for i = 1:numel(imgDirs)
-	[images1, img_resized1, x1, labels1, fns1] = read_labeled_chairs([imgBaseDir imgDirs{i} '/'], scaledSize, false, 2, false);
-	[images, img_resized, x, labels, fns] = merge_chairs_data(images, img_resized, x, labels, fns, images1, img_resized1, x1, labels1, fns1);
+	[images1, img_resized1, x1, labels1, fns1, bad1] = read_labeled_chairs([imgBaseDir imgDirs{i} '/'], scaledSize, false, 2, false);
+	[images, img_resized, x, labels, fns, bad] = merge_chairs_data(images, img_resized, x, labels, fns, bad, images1, img_resized1, x1, labels1, fns1, bad1);
 end
 
 whos('images')
--- DL_chair/merge_chairs_data.m
+++ DL_chair/merge_chairs_data.m
@@ -1,7 +1,8 @@
-function [images, img_resized, x, labels, fns] = merge_chairs_data(images1, img_resized1, x1, labels1, fns1, images2, img_resized2, x2, labels2, fns2)
+function [images, img_resized, x, labels, fns, bad] = merge_chairs_data(images1, img_resized1, x1, labels1, fns1, bad1, images2, img_resized2, x2, labels2, fns2, bad2)
 	images = [images1 images2];
 	img_resized = cat(numel(size(img_resized1)), img_resized1, img_resized2);
 	x = [x1 x2];
 	labels = [labels1 labels2];
 	fns = [fns1 fns2];
+	bad = [bad1 bad2];
 end
