02016535470636e73767044a8b759a61bebd9e8e
nullspace <jxhchina@gmail.com>
2014-11-04 20:47:50 +0800

    msmp5

--- DL_chair/load_it.m
+++ DL_chair/load_it.m
@@ -1,6 +1,6 @@
 %scaledSize = 128;
 imgBaseDir = '../../images/';
-imgDirs = {'chair_labeled_97_png', 'yes', 'msmp2', 'msmp3', 'msmp4'};
+imgDirs = {'chair_labeled_97_png', 'yes', 'msmp2', 'msmp3', 'msmp4', 'msmp5'};
 images = {};
 img_resized = {};
 x = [];
@@ -13,8 +13,10 @@ for i = 1:numel(imgDirs)
 	[images, img_resized, x, labels, fns, bad] = merge_chairs_data(images, img_resized, x, labels, fns, bad, images1, img_resized1, x1, labels1, fns1, bad1);
 end
 
-whos('images')
-whos('x')
+%whos('images')
+%whos('x')
+fprintf('read: %d\n', numel(fns));
+fprintf('bad : %d\n', numel(bad));
 
 disp('saving...')
 save -v7.3 ../../data/chairs_data_5sets.mat images img_resized x labels fns
