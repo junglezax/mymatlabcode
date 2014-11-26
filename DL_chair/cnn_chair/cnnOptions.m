5c789693ff9626e73027ddeddc037a4474221a54
nullspace <jxhchina@gmail.com>
2014-11-26 20:26:03 +0800

    merge

--- DL_chair/cnn_chair/cnnOptions.m
+++ DL_chair/cnn_chair/cnnOptions.m
@@ -9,26 +9,6 @@ options.patchDim = 8;
 options.poolDim = 24;          % dimension of pooling region % (imageDim - patchDim + 1)/poolDim = int
 options.imageChannels = 3;     % number of channels (rgb, so 3)
 options.numPatches = 100000;   % number of patches
-options.hiddenSize  = 20;           % number of hidden units 
-options.stepSize = 10; % step size for cnnConvolve and pooling, hiddenSize / stepSize = int
-
-options.sparsityParam = 0.035; % desired average activation of the hidden units.
-options.lambda = 3e-3;         % weight decay parameter       
-options.beta = 5;              % weight of sparsity penalty term
-
-options.epsilon = 0.1;	       % epsilon for ZCA whitening
-<<<<<<< HEAD
-n options = cnnOptions(useSAE)
-
-if ~exist('useSAE', 'var')
-	useSAE = false;
-end
-
-options.imageDim = 487;
-options.patchDim = 8;
-options.poolDim = 24;          % dimension of pooling region % (imageDim - patchDim + 1)/poolDim = int
-options.imageChannels = 3;     % number of channels (rgb, so 3)
-options.numPatches = 100000;   % number of patches
 options.hiddenSize  = 10;           % number of hidden units 
 options.stepSize = 10; % step size for cnnConvolve and pooling, hiddenSize / stepSize = int
 
@@ -37,58 +17,9 @@ options.lambda = 3e-3;         % weight decay parameter
 options.beta = 5;              % weight of sparsity penalty term
 
 options.epsilon = 0.1;	       % epsilon for ZCA whitening
-options.maxIter = 400;
-
-options.softmaxIter = 200;
-options.softmaxLambda = 1e-4;
-
-options.labelLevel = 1;
-options.save = false;
-options.display = 'Off';
-
-%imgDirs = {'png97', 'yes', 'msmp1', 'msmp2', 'msmp3', 'msmp4', 'msmp5', 'msmp6', 'msmp7', 'msmp8', 'msmp9', 'msmp10', 'msmp11', 'msmp12', 'msmp13', 'msmp14'};
-options.imgDir = '../../../images/chairs'; % maybe not used
-options.dataDir = '../../../data';
-
-options.classMethod = 'softmax'; % softmax/svm
-
-
-if useSAE
-options.sparsityParam = 0.1;
-options.beta = 3;
-options.softmaxIter = 100;
-
-options.hiddenSizeL1 = 100;
-options.hiddenSizeL2 = 100;
-end
-
-% for debug
-if 1
-options.imgDir = '../../../images/nope';
-options.imageDim = 15;
-options.patchDim = 4;
-options.poolDim = 4;          % dimension of pooling region % (imageDim - patchDim + 1)/poolDim = int
-options.imageChannels = 3;     % number of channels (rgb, so 3)
-options.numPatches = 10;   % number of patches
-options.hiddenSize  = 4;           % number of hidden units 
-options.stepSize = 2; % hiddenSize / stepSize = int
-options.maxIter = 10;
-options.softmaxIter = 5;
-end
-
-options.numClasses = code2label(options.labelLevel);
-
-assert(mod(options.hiddenSize, options.stepSize) == 0, 'stepSize should divide hiddenSize');
-assert(mod((options.imageDim - options.patchDim + 1), options.poolDim) == 0, 'poolDim should divide (imageDim - patchDim + 1)');
-
-end
-
 options.maxIter = 200;
-=======
-options.maxIter = 400;
 
->>>>>>> f9cec0bc47828566b407644c403e93a1db660f4a
-options.softmaxIter = 200;
+options.softmaxIter = 100;
 options.softmaxLambda = 1e-4;
 
 options.labelLevel = 1;
