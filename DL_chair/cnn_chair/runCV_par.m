582cb047b14bbca05afdcea1cc5d838f208e5255
nullspace <jxhchina@gmail.com>
2014-12-02 21:34:18 +0800

    ...

--- DL_chair/cnn_chair/runCV_par.m
+++ DL_chair/cnn_chair/runCV_par.m
@@ -4,11 +4,14 @@ function [accTests, data_small] = runCV_par(k, data)
 	%        tic; [accTests, data_small] = runCV_par(data); toc
 	%        tic; [accTests, data_small] = runCV_par(20, data); toc
 	
+	%matlab -nosplash
 	coreNum = 12;
 	if matlabpool('size') <= 0
 		disp('opening matlabpool....');
 		matlabpool('open', 'local', coreNum);
 	else
+		% matlabpool close
+		% matlabpool('open', 'local', coreNum);
 		disp('Already initialized');
 	end
 
